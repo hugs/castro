58f1e9b10c773fdaa4e9d7d07297da7445d3af7e
Jason Huggins <jason@jrandolph.com>
2009-04-17 14:03:40 -0500

    Refactoring creating of recording process to allow restarting.

--- __init__.py
+++ __init__.py
@@ -11,21 +11,28 @@ from lib.pyvnc2swf import vnc2swf
 # Get directory for storing files:
 storage_dir = os.environ.get('CASTRO_DATA_DIR',
                              tempfile.gettempdir()) 
-# Create a vnc process handler
-vnc = Process(target= vnc2swf.main, args=[[
-                    'lib/pyvnc2swf/vnc2swf.py', 
-                    '-n', 
-                    '-o', os.path.join(storage_dir, 'video.swf'),
-                    'localhost:0']])
+#
 # Public functions
+#
+def init():
+    global vnc
+    vnc = Process(target= vnc2swf.main, args=[[
+                        'lib/pyvnc2swf/vnc2swf.py', 
+                        '-n', 
+                        '-o', os.path.join(storage_dir, 'video.swf'),
+                        'localhost:0']])
 def start():
     vnc.start()
 
 def stop():
-    # Communicate with vnc2swf by telling it to stop looping
     mb.loop.write(False)
 
-# For testing only, really
+def restart():
+    stop()
+    init()
+    start()
+
+# Create some dummy output during a test
 def _make_some_test_output():
     sys.stdout.write("\nRecording a 10 second video...\n\n")
     for i in range(11):
@@ -35,6 +42,7 @@ def _make_some_test_output():
     sys.stdout.write("\n")
 
 def test():
+    init()
     start()
     _make_some_test_output()
     stop()
