4a9b06f5865bc14a3296195a16db7de81becfb76
Jason Huggins <jason@jrandolph.com>
2009-04-17 13:31:38 -0500

    Code cleanup; added some comments

--- castro.py
+++ castro.py
@@ -3,7 +3,7 @@ import sys
 import tempfile
 import time
 
-from processing import Process, Queue
+from processing import Process
 
 import lib.messageboard as mb
 from lib.pyvnc2swf import vnc2swf
@@ -11,29 +11,31 @@ from lib.pyvnc2swf import vnc2swf
 # Get directory for storing files:
 storage_dir = os.environ.get('CASTRO_DATA_DIR',
                              tempfile.gettempdir()) 
+# Create a vnc process handler
 vnc = Process(target= vnc2swf.main, args=[[
                     'lib/pyvnc2swf/vnc2swf.py', 
                     '-n', 
                     '-o', os.path.join(storage_dir, 'video.swf'),
                     'localhost:0']])
-
+# Public functions
 def start():
     vnc.start()
 
-def make_some_test_output():
+def stop():
+    # Communicate with vnc2swf by telling it to stop looping
+    mb.loop.write(False)
+
+# For testing only, really
+def _make_some_test_output():
     for i in range(10):
         sys.stdout.write("%s " % i)
         sys.stdout.flush()
         time.sleep(1)
 
-def stop():
-    mb.loop.write(False)
-
 def main():
     start()
-    make_some_test_output()
+    _make_some_test_output()
     stop()
-
 
 if __name__ == '__main__':
     main()
