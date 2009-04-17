172af63fed7ae0047f7f63100396580e62eba051
Jason Huggins <jason@jrandolph.com>
2009-04-17 13:47:40 -0500

    Renamed castro.py to __init__.py in order to simplify package imports from other places.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,43 @@
-#
+import os
+import sys
+import tempfile
+import time
+
+from processing import Process
+
+import lib.messageboard as mb
+from lib.pyvnc2swf import vnc2swf
+
+# Get directory for storing files:
+storage_dir = os.environ.get('CASTRO_DATA_DIR',
+                             tempfile.gettempdir()) 
+# Create a vnc process handler
+vnc = Process(target= vnc2swf.main, args=[[
+                    'lib/pyvnc2swf/vnc2swf.py', 
+                    '-n', 
+                    '-o', os.path.join(storage_dir, 'video.swf'),
+                    'localhost:0']])
+# Public functions
+def start():
+    vnc.start()
+
+def stop():
+    # Communicate with vnc2swf by telling it to stop looping
+    mb.loop.write(False)
+
+# For testing only, really
+def _make_some_test_output():
+    sys.stdout.write("\nRecording a 10 second video...\n\n")
+    for i in range(11):
+        sys.stdout.write("%s " % i)
+        sys.stdout.flush()
+        time.sleep(1)
+    sys.stdout.write("\n")
+
+def test():
+    start()
+    _make_some_test_output()
+    stop()
+
+if __name__ == '__main__':
+    test()
