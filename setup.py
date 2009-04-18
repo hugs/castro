5d3ed298656bc1de8edc313f120fe1e3b84b342d
Jason Huggins <jason@jrandolph.com>
2009-04-18 01:16:46 -0500

    Added dependency information

--- setup.py
+++ setup.py
@@ -7,22 +7,31 @@ setup(name='castro',
       version=version,
       description="Screencasting library",
       long_description="""\
+
 Castro is a library for recording automated screencasts.
 
-Castro is a minor fork of pyvnc2swf, allowing one to use pyvnc2swf as a 
-regular Python library, instead of a Tk GUI application or command line 
-utility. 
+**Summary**
+  Castro is a minor fork of pyvnc2swf, allowing one to use pyvnc2swf as a 
+  regular Python library, instead of a Tk GUI application or command line 
+  utility. 
 
-The specific improvement Castro brings to pyvnc2swf is the ability to start 
-and stop recording programmatically via a regular Python API. Castro 
-uses a simple file-based IPC to tell pyvnc2swf when to stop recording.
+  The specific improvement Castro brings to pyvnc2swf is the ability to start 
+  and stop recording programmatically via a regular Python API. Castro 
+  uses a simple file-based IPC to tell pyvnc2swf when to stop recording.
 
-Ordinarily, pyvnc2swf's command line utility, vnc2swf.py, expects users to 
-stop recording by manually typing "Control-C", sending a KeyboardInterrupt 
-and allowing the process to exit cleanly. On Linux, emulating KeyboardInterrupt
-is simple enough to do by sending a SIGINT signal. But this does not work cross-
-platform, specifically on Windows. And a big reason for using pyvnc2swf is its
-ability to record vnc video on any platform *from* any platform.
+  Ordinarily, pyvnc2swf's command line utility, vnc2swf.py, expects users to 
+  stop recording by manually typing "Control-C", sending a KeyboardInterrupt 
+  and allowing the process to exit cleanly. On Linux, emulating KeyboardInterrupt
+  is simple enough to do by sending a SIGINT signal. But this does not work cross-
+  platform, specifically on Windows. And a big reason for using pyvnc2swf is its
+  ability to record vnc video on any platform *from* any platform.
+
+**Dependencies**
+  Python - 2.5 or above
+  Pygame - 1.6 or above (http://www.pygame.org/download.shtml)
+  Processing - 0.52 or above
+  Simplejson - 2.0.9 or above
+
 """,
       # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
